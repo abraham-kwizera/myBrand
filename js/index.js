e561c954f7429e061b8805b1486faccdfb76723d
abraham-kwizera <kwiz.abraham4@gmail.com>
2022-02-23 03:11:17 +0200

    Add firebase authentication for user login

--- js/index.js
+++ js/index.js
@@ -128,6 +128,48 @@ function register() {
 
 }
 
+// Set up our login function
+function login() {
+    // Get all our input fields
+    email = document.getElementById('email').value;
+    password = document.getElementById('password').value;
+
+    // Validate input fields
+    if (validate_email(email) == false || validate_password(password) == false) {
+        alert('Email or Password is Outta Line!!');
+        return;
+        // Don't continue running the code
+    }
+
+    auth.signInWithEmailAndPassword(email, password)
+        .then(function() {
+            // Declare user variable
+            var user = auth.currentUser;
+
+            // Add this user to Firebase Database
+            var database_ref = database.ref();
+
+            // Create User data
+            var user_data = {
+                last_login: Date.now()
+            };
+
+            // Push to Firebase Database
+            database_ref.child('users/' + user.uid).update(user_data);
+
+            // DOne
+            alert('Log in successfully');
+            window.location.href = 'http://localhost:5500/admin';
+        })
+        .catch(function(error) {
+            // Firebase will use this to alert of its errors
+            var error_code = error.code;
+            var error_message = error.message;
+
+            alert(error_message);
+        })
+}
+
 
 
 
